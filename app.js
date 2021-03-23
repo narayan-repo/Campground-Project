eee00aa336c4287a354382c0a217cdfa345f6096
narayan-repo <https://github.com/narayan-repo>
2021-03-23 16:58:02 +0530

    include bootstrap and add basic styling

--- app.js
+++ app.js
@@ -5,8 +5,32 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine','ejs');
-
+app.use(express.static('public'))
 let campgrounds = [
+    {
+        name: "Thanda Tirpol",
+        img : "https://images.unsplash.com/photo-1533597818151-d1071f26fe32?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtcGdyb3VuZHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name: "Gorom Baashbagan",
+        img: "https://images.unsplash.com/photo-1534685157449-86b12aed151e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FtcGdyb3VuZHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name: "Neutral Room",
+        img: "https://images.unsplash.com/photo-1564084372010-3f5f8c6c095a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FtcGdyb3VuZHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name: "Thanda Tirpol",
+        img : "https://images.unsplash.com/photo-1533597818151-d1071f26fe32?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtcGdyb3VuZHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name: "Gorom Baashbagan",
+        img: "https://images.unsplash.com/photo-1534685157449-86b12aed151e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FtcGdyb3VuZHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name: "Neutral Room",
+        img: "https://images.unsplash.com/photo-1564084372010-3f5f8c6c095a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FtcGdyb3VuZHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
     {
         name: "Thanda Tirpol",
         img : "https://images.unsplash.com/photo-1533597818151-d1071f26fe32?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtcGdyb3VuZHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
@@ -34,7 +58,7 @@ app.post('/campgrounds', (req, res) => {
     const url = req.body.url;
     const newCampground = {name: name,img: url}
     campgrounds.push(newCampground);
-    res.redirect('/')
+    res.redirect('/campgrounds')
 })
 
 app.get('/campgrounds/new',(req, res) => {
